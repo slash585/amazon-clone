2b61f1d3f5c0448dd290208b4e4b2e6614347793
Mehmet Ali Özdemir <slsh585@gmail.com>
2021-03-01 02:45:55 +0300

    Create Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Rating from './Rating'
 
 export default function Product(props) {
     const { product } = props
@@ -11,23 +12,7 @@ export default function Product(props) {
         <a href={`/product/${product._id}`}>
             <h2>{ product.name }</h2>
           </a>
-          <div className="rating">
-            <span>
-              <i className="fa fa-star"></i>
-            </span>
-            <span>
-              <i className="fa fa-star"></i>
-            </span>
-            <span>
-              <i className="fa fa-star"></i>
-            </span>
-            <span>
-              <i className="fa fa-star"></i>
-            </span>
-            <span>
-              <i className="fa fa-star"></i>
-            </span>
-          </div>
+          <Rating rating={product.rating} numReviews={product.numReviews} />
           <div className="price">${product.price}</div>
         </div>
       </div>
