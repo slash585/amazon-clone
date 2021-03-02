446730cb613c66aec5570d24231df0e1fc84c053
Mehmet Ali Özdemir <slsh585@gmail.com>
2021-03-02 15:49:15 +0300

    Add redux to product view

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,13 @@ app.get('/api/products',(req,res)=>{
     res.send(data.products)
 })
 
+app.get('/api/products/:id',(req,res)=>{
+    const product = data.products.find(i => i._id == req.params.id)
+    if(product){
+        res.send(product)
+    }else{
+        res.status(404).send({message: 'Product not found'})
+    }
+})
+
 module.exports = app
--- frontend/src/actions/product-actions.js
+++ frontend/src/actions/product-actions.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/product-constant'
+import { PRODUCT_DETAIL_REQUEST, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_DETAIL_SUCCESS, PRODUCT_DETAIL_FAIL } from '../constants/product-constant'
 import axios from 'axios'
 
 export const listProducts = () => async (dispatch) => {
@@ -11,5 +11,18 @@ export const listProducts = () => async (dispatch) => {
         dispatch({type: PRODUCT_LIST_SUCCESS, payload: data})
     }catch(e){
         dispatch({type: PRODUCT_LIST_FAIL, payload: e.message})
+    }
+}
+
+export const detailsProduct = (productId) => async (dispatch) => {
+    dispatch({type: PRODUCT_DETAIL_REQUEST, payload: productId})
+    try{
+        const { data } = await axios.get(`http://localhost:5000/api/products/${productId}`)
+        dispatch({ type:PRODUCT_DETAIL_SUCCESS, payload:data})
+    }catch(e){
+        dispatch({ type:PRODUCT_DETAIL_FAIL, payload:e.response &&  e.response.data.message
+            ? e.response.data.message
+            : e.message,
+        })
     }
 }
