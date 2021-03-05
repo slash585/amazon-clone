e66bd3ab07611bc4b1ced185765c4e0463c44d0b
Mehmet Ali Özdemir <slsh585@gmail.com>
2021-03-06 01:48:19 +0300

    Design place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { signout } from './actions/user-actions';
 import ShippingAdressScreen from './views/shipping-adress-screen';
 import PaymentScreen from './views/payment-method-screen'
-
+import PlaceOrderScreen from './views/place-order-screen'
 
 function App() {
   const cart = useSelector(state => state.cart)
@@ -63,6 +63,7 @@ function App() {
         <Route path="/register" component={RegisterScreen}></Route>
         <Route path="/shipping" component={ShippingAdressScreen}></Route>
         <Route path="/payment" component={PaymentScreen}></Route>
+        <Route path="/placeorder" component={PlaceOrderScreen}></Route>
         <Route path="/" component={HomeScreen} exact></Route>
       </main>
       <footer className="row center">All right reserved</footer>
